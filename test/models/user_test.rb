ed9174ec4ba7da6e7161c9a1b81fba76deef51a7
KSheehan <ksheehan927@gmail.com>
2017-08-04 08:40:21 -0400

    User model tests, stylize forms

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,35 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?
   end
     
+  test "first should be present" do
+    @user.first = ""
+    assert_not @user.valid? 
+  end
+    
+  test "last should be present" do
+    @user.last = ""
+    assert_not @user.valid? 
+  end
+    
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid? 
+  end
+    
+  test "password must be nonblank" do
+    @user.password = "     "
+    assert_not @user.valid? 
+  end
+    
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid? 
+  end
+    
+  test "password should match psasword_confirmation" do
+    @user.password = "a" * 6
+    @user.password_confirmation = "b" * 6
+    assert_not @user.valid? 
+  end
+    
 end
