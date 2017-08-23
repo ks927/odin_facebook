fbd355688b179e7e7798410365a995d6c7e1d4a1
KSheehan <ksheehan927@gmail.com>
2017-08-22 22:27:05 -0400

    Stylize comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,11 +8,5 @@ class CommentsController < ApplicationController
 
   def destroy
   end
-
-  private
-    
-  #def comment_params
-   #  params.require(:comment).permit(user_id: current_user.id, :content)
-  #end
     
 end
