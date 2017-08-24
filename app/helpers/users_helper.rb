119f630970d2639c70dd1636055c4b1b75735591
KSheehan <ksheehan927@gmail.com>
2017-08-24 15:52:42 -0400

    User info and friends to user page with styles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
     
     # Returns gravatar for given user
-    def gravatar_for(user, size: 80)
+    def gravatar_for(user, size: 20)
       gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
       gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
       image_tag(gravatar_url, alt: user.first, class: "gravatar")
