52b23f4ae24e7c57f86904cf54a32e8faf2913d6
KSheehan <ksheehan927@gmail.com>
2017-08-31 13:26:25 -0400

    Responsive Ajax but only affecting first post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,16 +10,16 @@ class LikesController < ApplicationController
       current_user.liked_posts << @post
       respond_to do |format|
         format.js 
-        format.html { redirect_to @post }
+        format.html { redirect_to posts_path }
       end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    like = current_user.likes.where(post: @post).first
+    like = current_user.likes.find_by(post_id: params[:post_id])
     Like.destroy(like)
     respond_to do |format|
-      format.html { redirect_to @post }
+      format.html { redirect_to posts_path }
       format.js 
     end
   end
