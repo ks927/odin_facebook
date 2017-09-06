09600c2a898a7186a2ac0a0fdfb30890bba99d8f
KSheehan <ksheehan927@gmail.com>
2017-09-06 17:14:57 -0400

    Profile page timeline css fix

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,13 @@
 class LikesController < ApplicationController
+    respond_to :js
     
   def create
       @post = Post.find(params[:post_id])
       #like = current_user.likes.build(post_id: params[:post_id])
       current_user.liked_posts << @post
       respond_to do |format|
-        format.js 
-        format.html { redirect_to request.referrer || root_url }
+        format.js
+        format.html { redirect_to :back }
       end
   end
 
