351e40ddd385d9ad35130f999dd1dade0ac55ab4
KSheehan <ksheehan927@gmail.com>
2017-09-15 16:51:46 -0400

    Add region to S3 credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
     storage: :s3,
     s3_region: ENV["S3_REGION"],
     s3_credentials: {
-      s3_host_name: ENV["AWS_S3_HOST_NAME"],
+      s3_host_name: 's3-us-east-2.amazonaws.com',
       bucket: ENV["S3_BUCKET"],
       access_key_id: ENV["S3_ACCESS_KEY"],
       secret_access_key: ENV["S3_SECRET_KEY"]
